97ba4fa64d4c1cedb435146af56aeb6443d29c89
Daniel Shimield <dxshim90@gmail.com>
2019-01-25 12:31:19 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
 def say_hello(name = "Ruby Programmer")
   puts "Hello #{name}!"
+=======
+def say_hello(Name = "Ruby Programmer!")
+  print "Hello #{Name}"
+>>>>>>> 63f50131173df9201fa695969e25d91d75667351
 end
